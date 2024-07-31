91cb7232e29c043aaabf3372d230003194bd0828
Fea <feathecutie@disroot.org>
2024-07-31 15:30:56 +0200

    json2kdl: update

--- pkgs/by-name/js/json2kdl/package.nix
+++ pkgs/by-name/js/json2kdl/package.nix
@@ -11,14 +11,14 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "AgathaSorceress";
     repo = pname;
-    rev = "ddef0dfe54db1dfbc96081b15ee6dd6ef2a807d3";
-    hash = "sha256-zQCC5v2WmBAgso6IbLE2qgIzOdeUBxAElEvfYGjoC9M=";
+    rev = "285e1e8aff1248da5fdb32165af9286faa1739e2";
+    hash = "sha256-tkr7jOYgLtcOV13pz4/DzbcSp2IbuOAvmMS86wkaNw4=";
   };
 
   cargoHash = "sha256-jDdVSCtRp6heE72+bxF6ixaYNbJpKkCDg4mQskbQoZc=";
 
   meta = {
-    description = "A program that converts JSON files to KDL";
+    description = "Program that converts JSON files to KDL";
     homepage = "https://github.com/AgathaSorceress/json2kdl";
     platforms = lib.platforms.all;
     maintainers = (with lib.maintainers; [ feathecutie ]);
